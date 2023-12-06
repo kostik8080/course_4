7f22f94256984e7c3f8f9b82ddcd568b97993143
Konstantin Onishchenko <kostik80_80@mail.ru>
2023-12-06 19:19:49 +0300

    Формат катекорий по вакансии

--- src/utils.py
+++ src/utils.py
@@ -24,6 +24,7 @@ class HeadHunter(Engine):
     """
     Класс API по hh.ru
     """
+
     def __init__(self, keyword):
         self.keyword = keyword
         self.__header = {
@@ -101,7 +102,6 @@ class SuperJob(Engine, ABC):
             formatted_salary = salary if currency == 'rub' else salary['from'] * 76
         return formatted_salary
 
-
     def get_request(self):
         response = requests.get("https://api.superjob.ru/2.0/vacancies/",
                                 headers=self.__header,
@@ -110,4 +110,28 @@ class SuperJob(Engine, ABC):
             raise ParsingError
         return response.json()['objects']
 
+    def get_formatted_vacancies(self):
+        formatted_vacancies = []
+        for vacancy in self.__vacancies:
+            formatted_vacancies.append({
+                "id": vacancy["id"],
+                "title": vacancy["profession"],
+                "url": vacancy["link"],
+                "salary_from": self.get_salary(vacancy['payment_from'], vacancy['currency']),
+                "salary_to": self.get_salary(vacancy['payment_to'], vacancy['currency']),
+                "employer": vacancy['firm_name'],
+                "api": "SuperJob",
+            })
+        return formatted_vacancies
 
+    def get_vacancies(self, pages_count=1):
+        while self.__params['page'] < pages_count:
+            print(f"SuperJob, Парсинг страницы {self.__params['page'] + 1}", end=": ")
+            try:
+                values = self.get_request()
+            except ParsingError:
+                print("Ошибка при получении данных")
+                break
+            print(f"Найдено ({len(values)}) вакансий.")
+            self.__vacancies.extend(values)
+            self.__params['page'] += 1
