56c8075e897e7de1bb26c6189fe11d5cd54bb9e9
Konstantin Onishchenko <kostik80_80@mail.ru>
2023-12-06 18:25:45 +0300

    Получение значений через API hh.ru

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 from abc import ABC, abstractmethod
+
+import requests
+
+class ParsingError(Exception):
+    def __str__(self):
+        return "Ошибка подключения по API"
+
+
 class Engine(ABC):
     """Абстрактный класс"""
     @abstractmethod
@@ -8,3 +16,26 @@ class Engine(ABC):
 
     def get_vacancies(self):
         pass
+
+
+class HeadHunter(Engine):
+    def __init__(self, keyword):
+        self.keyword = keyword
+        self.__header = {
+            "User-Agent": "kostik80_80@mail.ru"}
+        self.__params = {
+            "text": keyword,
+            "page": 0,
+            "per_page": 100,
+        }
+        self.__vacancies = []  # список вакансий, который заполняется  по мерере получения данных по api
+
+
+    def get_request(self):
+        """
+        Получение значений через API
+        """
+        response = requests.get("https://api.hh.ru/vacancies", headers=self.__header, params=self.__params)
+        if response.status_code != 200:
+            raise ParsingError
+        return response.json()["items"]
