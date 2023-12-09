7a4332a5e802f4fd9b66d12ef5146db56ee402ee
Konstantin Onishchenko <kostik80_80@mail.ru>
2023-12-09 14:04:24 +0300

    Вывод сразу с двух платформ

--- src/main.py
+++ src/main.py
@@ -11,29 +11,29 @@ def main():
         api.get_vacancies(pages_count=10)
         vacancies_json.extend(api.get_formatted_vacancies())
 
-        connector = Connector(keyword=keyword, vacancies_json=vacancies_json)
+    connector = Connector(keyword=keyword, vacancies_json=vacancies_json)
 
-        while True:
-            command = input(
-                '1 - Вывести список вакансий;\n'
-                '2 - Отсортировать по возврастанию дохода;\n'
-                '3 - Отсортировать по убыванию дохода;\n'
-                '0 - Выход;\n'
-            )
-            if command.lower() == "0":
-                break
-            elif command == "1":
-                vacancies = connector.select()
-            elif command == "2":
-                vacancies = connector.sorted_vacancies_by_salary_from_asc()
-            elif command == "3":
-                vacancies = connector.sorted_vacancies_by_salary_to_asc()
-            else:
-                print("Не корректные данные")
-                exit()
+    while True:
+        command = input(
+            '1 - Вывести список вакансий;\n'
+            '2 - Отсортировать по возврастанию дохода;\n'
+            '3 - Отсортировать по убыванию дохода;\n'
+            '0 - Выход;\n'
+        )
+        if command.lower() == "0":
+            break
+        elif command == "1":
+            vacancies = connector.select()
+        elif command == "2":
+            vacancies = connector.sorted_vacancies_by_salary_from_asc()
+        elif command == "3":
+            vacancies = connector.sorted_vacancies_by_salary_to_asc()
+        else:
+            print("Не корректные данные")
+            exit()
 
-            for vacancy in vacancies:
-                print(vacancy, end="\n\n")
+        for vacancy in vacancies:
+            print(vacancy, end="\n\n")
 
 
 if __name__ == "__main__":
